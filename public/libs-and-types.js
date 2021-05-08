9d0aaa1b6717a3ae49b0d266d82f943c53c1cc25
keaton <keatonrgallagher@gmail.com>
2021-05-08 16:00:23 -0600

    Packages

---
+++ public/libs-and-types.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// You must also intsall @types/react to get the types npm i @types/react
---
+++ src/libs-and-types.ts
@@ -0,0 +1,4 @@
+// Installing types
+// npm i - D @types/npm-package-name
+import React from 'react'
+// You must also intsall @types/react to get the types npm i @types/react
