c1a2116d58d2ee122e0e6a82b2cd33efd988759d
keaton <keatonrgallagher@gmail.com>
2021-05-08 19:04:51 -0600

    Getting started with Conditional types

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-// Mapped Types https://www.udemy.com/course/react-with-typescript/learn/lecture/19895226#overview
-
-type Props = 'propA' | 'propB';
-type MyMappedType<T> = {
-    [P in keyof T]: T[P] | null
-};
-
-type myNewType = MyMappedType<{ a: 'a'; b: 'b' }>;
-
-type Pick1<T, Props extends keyof T> = {
-    [P in Props]: T[P]
-}
-
-type myNewType2 = Pick1<{ a: 'a'; b: 'b' }, 'a'>
+// Conditional Types https://www.udemy.com/course/react-with-typescript/learn/lecture/19163254#overview
